95163e535113fc333c8f5124a3db738b0b0a44f9
Shivram <kashyapshivram512@gmail.com>
2024-09-13 18:54:58 +0530

    aa

--- utils/store.js
+++ utils/store.js
@@ -1,6 +1,6 @@
 'use client'
 import { createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from "@redux-devtools/extension";
 import RootReducer from "./reducer";
 
 const store = createStore(RootReducer, composeWithDevTools());
