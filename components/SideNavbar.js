ca3eafac95468d9467f71bf2a4a2e9ff0a7b380d
Shivram <kashyapshivram512@gmail.com>
2024-09-13 18:50:35 +0530

    aa

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -29,9 +29,9 @@ function SideNavbar() {
               Virtual Dashboard
             </h1>
             <div className=" my-4 border-b border-gray-100 pb-4">
-              <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+              <div style={{background: "black"}} className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white click" />
+                <h3 style={{color: "white"}} className="text-base text-gray-800 group-hover:text-white font-semibold ">
                   Dashboard
                 </h3>
               </div>
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,12 +1,7 @@
 import Head from "next/head";
-import SideNavbar from "../components/SideNavbar";
-import { useDispatch, useSelector } from 'react-redux';
-import { useRouter } from "next/router";
+import SideNavbar from "../../components/SideNavbar";
 
 export default function Home() {
-  const dispatch = useDispatch()
-  const router = useRouter()
-  const { userDetails } = useSelector(state => state)
   return (
     <div className="bg-blue-800">
       <Head>
@@ -16,7 +11,7 @@ export default function Home() {
       </Head>
       <SideNavbar />
       <div style={{padding: "1px 396px"}}>
-        <h1>Hello Shivram : {userDetails.userType}</h1>
+        <h1>Welcome Home</h1>
       </div>
     </div>
   );
