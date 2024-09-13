df27720f72a127c8a553e662a4a59a19faac9d23
Shivram <kashyapshivram512@gmail.com>
2024-09-13 19:30:05 +0530

    s4ffff

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -26,7 +26,7 @@ function SideNavbar() {
         <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start item-center">
             <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
-              Virtual Dashboard
+              Virtual Dashboard Test
             </h1>
             <div className=" my-4 border-b border-gray-100 pb-4">
               <div style={{background: "black"}} className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
